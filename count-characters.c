64ac2bafca7bebb5fffbc5ea8d109b7c2e0144a8
Herbert Lin <herbert.lin.7@gmail.com>
2023-09-13 20:33:03 -0700

    Add note on how to reach EOF

--- count-characters.c
+++ count-characters.c
@@ -21,3 +21,9 @@ void countCharactersV2()
         ;
     printf("%.0f\n", nc);
 }
+
+/*
+Note:
+To reach EOF on windows subsystem linux ubuntu you need to input Ctrl-D twice
+Ref - https://stackoverflow.com/questions/12134756/character-counter-from-the-c-programming-language-not-working-as-i-expected
+*/
